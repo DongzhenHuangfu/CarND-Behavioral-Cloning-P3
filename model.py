60fa017369da6489c3074e134e117ea2a453b9aa
Dongzhen <wyatt_h@outlook.com>
2018-04-16 10:30:20 +0200

    refactor: rebuild the Network structure, add the pictures from the left and right side camera as input

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.switch_backend('agg')
 from keras.models import Sequential, Model
-from keras.layers import Flatten, Dense, Lambda, Cropping2D
+from keras.layers import Flatten, Dense, Lambda, Cropping2D, Activation, Dropout
 from keras.layers.convolutional import Convolution2D
 from keras.layers.pooling import MaxPooling2D
 import sklearn
@@ -31,13 +31,28 @@ def generator(lines, batch_size=32):
 			images = []
 			measurements = []
 			for line in batch_samples:
-				source_path = line[0]
-				filename = source_path.split('/')[-1]
-				current_path = './data/IMG/' + filename
-				image = cv2.imread(current_path)
-				images.append(image)
-				measurement = float(line[3])
-				measurements.append(measurement)
+				correction = 0.3
+
+				source_path_center = line[0]
+				source_path_left = line[1]
+				source_path_right = line[2]
+
+				filename_center = source_path_center.split('/')[-1]
+				filename_left = source_path_left.split('/')[-1]
+				filename_right = source_path_right.split('/')[-1]
+
+				current_path_center  = './data/IMG/' + filename_center
+				current_path_left = './data/IMG/' + filename_left
+				current_path_right = './data/IMG/' + filename_right
+
+				image_center = cv2.imread(current_path_center)
+				image_left = cv2.imread(current_path_left)
+				image_right = cv2.imread(current_path_right)
+
+				images.extend([image_center, image_left, image_right])
+
+				measurement_center = float(line[3])
+				measurements.extend([measurement_center, measurement_center+correction, measurement_center-correction])
 
 			augmented_images, augmented_measurements = [], []
 			for image,measurement in zip(images,measurements):
@@ -60,19 +75,25 @@ model.add(Lambda(lambda x: x / 127.5 - 1.0,
 model.add(Cropping2D(cropping=((75,25),(0,0))))
 model.add(Convolution2D(24,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(36,5,5,subsample=(2,2),activation="relu"))
+model.add(Convolution2D(48,5,5,subsample=(2,2),activation="relu"))
 model.add(Convolution2D(64,3,3,activation="relu"))
-model.add(Convolution2D(64,3,3,activation="relu"))
+model.add(Convolution2D(64,2,2,activation="relu"))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(100))
+model.add(Activation('relu'))
 model.add(Dense(50))
+model.add(Activation('relu'))
 model.add(Dense(10))
+model.add(Activation('relu'))
+model.add(Dense(3))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 # model.fit(X_train,y_train, validation_split=0.2,shuffle=True, nb_epoch=5)
 history_object = model.fit_generator(train_generator, samples_per_epoch=len(train_samples), \
 									validation_data=validation_generator, \
-									nb_val_samples=len(validation_samples), nb_epoch=8, verbose=1)
+									nb_val_samples=len(validation_samples), nb_epoch=20, verbose=1)
 
 plt.plot(history_object.history['loss'])
 plt.plot(history_object.history['val_loss'])
